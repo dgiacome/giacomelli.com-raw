c03b93b333cc7caa20037c44346d7f00d2c58173
Pr0x1 <36932681+dgiacome@users.noreply.github.com>
2023-05-01 16:59:10 -0400

    Add files via upload

---
+++ cignaspoof/_vti_cnf/analytics.js
@@ -0,0 +1,12 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TW|22 Apr 2017 15:26:00 -0000
+vti_author:SR|TUFPR0X1-2\\dan
+vti_modifiedby:SR|TUFPR0X1-2\\dan
+vti_nexttolasttimemodified:TW|22 Apr 2017 15:26:00 -0000
+vti_timecreated:TR|01 May 2023 20:31:50 -0000
+vti_cacheddtm:TX|01 May 2023 20:31:50 -0000
+vti_filesize:IR|3648
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|cignaspoofifp.htm
+vti_syncwith_home131756646.1and1-data.host\:990:TW|01 May 2023 20:31:50 -0000
+vti_syncofs_home131756646.1and1-data.host\:990:TW|22 Apr 2017 15:26:00 -0000
